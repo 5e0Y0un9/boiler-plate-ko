79587d21f3c800851aba5d8e2e73c3a43e79ff1f
박서영 <psy@bagseoyeong-ui-MacBook-Pro.local>
2020-08-17 23:36:31 +0900

    start

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const bodyParser = require("body-parser");
+const {User} = require("./models/User");
+
+//application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://box0:1023@boilerplate.sv3av.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -12,6 +20,19 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/regist', (req, res) => {
+   //회원 가입 할 때 필요한 정보들을 client에서 가져오면
+   //그것들을 데이터 베이스에 넣어준다.
+
+   const user = new User(req.body)
+
+   user.save((err, userInfo) => {
+     if(err) return res.json({ success: false, err})
+     return res.status(200).json({
+       success: true
+     })
+   })
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
